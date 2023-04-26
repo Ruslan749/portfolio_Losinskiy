b832d0020bac38e3b5e2f1dae3c64fee47f2b90f
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-04-26 22:39:51 +0500

    post

--- src/js/post.js
+++ src/js/post.js
@@ -21,34 +21,35 @@ function postData(form) {
         statusMessage.classList.add('status');
         statusMessage.textContent = message.loading;
         form.appendChild(statusMessage);
-    
-        const request = new XMLHttpRequest();
-        request.open('POST', 'server.php');
-        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
         const formData = new FormData(form);
 
         const object = {};
         formData.forEach(function(value, key){
             object[key] = value;
+            console.log(key + " = " + value)
         });
-        const json = JSON.stringify(object);
 
-        request.send(json);
-
-        request.addEventListener('load', () => {
-            if (request.status === 200) {
-                console.log(request.response);
-                statusMessage.textContent = message.success;
-                form.reset();
-                setTimeout(() => {
-                    statusMessage.remove();
-                }, 2000);
-            } else {
-                statusMessage.textContent = message.failure;
-                setTimeout(() => {
-                    statusMessage.remove();
-                }, 5000);
-            }
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        }).then(data => {
+            console.log(data);
+            statusMessage.textContent = message.success;
+            form.reset();
+            setTimeout(() => {
+                statusMessage.remove();
+            }, 2000)
+        }).catch(() => {
+            statusMessage.textContent = message.failure;
+            setTimeout(() => {
+                statusMessage.remove();
+            }, 5000);
+        }).finally(() => {
+            form.reset();
         });
     });
 }
